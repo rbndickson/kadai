35c2752fd8112b2d965f35166ee83fb15b1a8b55
Robin Dickson <robindickson@gmail.com>
2018-09-10 17:19:35 +0800

    user_logged_in?のhelper_methodを追加
    Add user_logged_in? helper_method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  helper_method :user_logged_in?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def user_logged_in?
+    !!current_user
+  end
 end
