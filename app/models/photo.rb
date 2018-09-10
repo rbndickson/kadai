1b57e0e593f99a9035b7ed0748381090515b1e21
Robin Dickson <robindickson@gmail.com>
2018-09-10 19:35:22 +0800

    userとphotoの関連付けを追加
    Add associations between users and photos
    
    　user has_many photos
    　photo belongs_to user

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,5 @@
 class Photo < ActiveRecord::Base
   validates :title, presence: true, length: { maximum: 30 }
+
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
 
   validates_presence_of :username, :password
   validates_uniqueness_of :username
+
+  has_many :photos
 end
