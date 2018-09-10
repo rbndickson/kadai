7aa45f2fb829c12cecf4d149cad677e58d9ac132
Robin Dickson <robindickson@gmail.com>
2018-09-10 18:03:50 +0800

    ユーザーログインを追加
    Add user login
    
    　SessionsControllerにcreateを追加
    　sessions/newのビューにflashのメッセージを追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,25 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    if params[:username].blank? || params[:password].blank?
+      flash.now[:missing_username] = "ユーザーIDが未入力" if params[:username].blank?
+      flash.now[:missing_password] = "パスワードが未入力" if params[:password].blank?
+
+      render :new
+    else
+      user = User.find_by(username: params[:username])
+
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+
+        redirect_to root_path
+      else
+        flash.now[:error] = "ユーザーIDとパスワードが一致するユーザーが存在しない"
+
+        render :new
+      end
+    end
+  end
 end
